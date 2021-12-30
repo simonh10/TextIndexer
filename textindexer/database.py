212a330a34f6ca5b36968159cd888220d6377ff8
Simon Hargreaves <simon@simon-hargreaves.com>
2021-12-30 16:20:49 +0000

    update db backend

---
+++ textindexer/database.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+import os
+
+
+class DocsDatabase():
+
+    _DATABASE = 'indexer'
+    _DOCUMENTS_COLLECTION = 'documents'
+    _INDEXES_COLLECTION = 'indexed'
+    _WORDS_COLLECTION = 'words'
+
+    def __init__(self, mongo={}):
+        mongo_config = {
+            'host': os.environ.get(
+                'MONGO_HOST',
+                mongo.get('host','mongo')),
+            'username': os.environ.get(
+                'MONGO_INITDB_ROOT_USERNAME',
+                mongo.get('username','mongo')),
+            'password': os.environ.get(
+                'MONGO_INITDB_ROOT_PASSWORD',
+                mongo.get('password','mongo'))}
+        self._db = MongoClient(
+            mongo_config.get('host'),
+            username=mongo_config.get('username'),
+            password=mongo_config.get('password'))[self._DATABASE]
+    
+    def get_documents(self, skip=0, limit=10):
+        docs_collection = self._db[self._DOCUMENTS_COLLECTION]
+        docs = docs_collection.find({})
+        count = docs.count()
+        docs.skip(skip).limit(limit)
+        ret_docs = []
+        for i in docs:
+            ret_docs.append(i)
+        return {
+            'count': count,
+            'documents': ret_docs}
+    
+    def add_document(self, document):
+        docs_collection = self._db[self._DOCUMENTS_COLLECTION]
+        docs_collection.insert_one(document)
--- textindexer/server/controllers/default_controller_implementation.py
+++ textindexer/server/controllers/default_controller_implementation.py
@@ -1,3 +1,6 @@
+from textindexer.database import DocsDatabase
+from textindexer.server.models.document_record import DocumentRecord  # noqa: E501
+import connexion
 
 
 def get_indexer_v1_document(skip=None, limit=None):  # noqa: E501
@@ -12,4 +15,23 @@ def get_indexer_v1_document(skip=None, limit=None):  # noqa: E501
 
     :rtype: Documents
     """
-    return 'implemetation output'
+    docs_db = DocsDatabase()
+    return docs_db.get_documents(skip=skip, limit=limit)
+
+
+def post_indexer_v1_document(document_record=None):
+    """post_indexer_v1_document
+
+    Add a document record # noqa: E501
+
+    :param document_record: 
+    :type document_record: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        document_record = DocumentRecord.from_dict(connexion.request.get_json())  # noqa: E501
+        docs_db = DocsDatabase()
+        docs_db.add_document(document_record.to_dict())
+
+    return None
